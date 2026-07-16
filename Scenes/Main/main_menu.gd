6bffe1533c4876d6ca22ac6102807faa3ee4ecd2
Regina Annemarie Bool <ninisalter@gmail.com>
2026-07-17 00:51:56 +0800

    Updated main menu file

--- Scenes/Main/main_menu.gd
+++ Scenes/Main/main_menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	%PlayButton.grab_focus()
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/LevelSelect.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_Select.tscn")
 
 func _on_quit_button_pressed() -> void:
 	if get_tree():
