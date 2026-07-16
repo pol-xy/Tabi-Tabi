48b73acf7a1a85193ccf95631bbea6182b8d27e4
Regina Annemarie Bool <ninisalter@gmail.com>
2026-07-17 01:38:02 +0800

    Fixed level selection wiring

--- Scenes/Level Selector/level_select.gd
+++ Scenes/Level Selector/level_select.gd
@@ -4,13 +4,13 @@ func _ready() -> void:
 	%Level1.grab_focus()
 
 func _on_level_1_button_pressed() -> void:
-	GameManager.current_level_index = 1
+	GameManager.current_level_index = 0
 	get_tree().change_scene_to_file("res://Scenes/Main_Jeepney.tscn")
 
 func _on_level_2_pressed() -> void:
-	GameManager.current_level_index = 2
+	GameManager.current_level_index = 1
 	get_tree().change_scene_to_file("res://Scenes/Main_Jeepney.tscn")
 	
 func _on_level_3_pressed() -> void:
-	GameManager.current_level_index = 3
+	GameManager.current_level_index = 2
 	get_tree().change_scene_to_file("res://Scenes/Main_Jeepney.tscn")
